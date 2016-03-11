ddc4a25aa490f5411bc9ac5441a9dd80074459bd
Duncan Macleod <duncan.macleod@ligo.org>
2016-03-11 14:07:29 -0800

    tests: added simple unit test for parameter files

--- omicron/tests/test_parameters.py
+++ omicron/tests/test_parameters.py
@@ -19,9 +19,16 @@
 """Test parameter handling for Omicron
 """
 
+import os
+from tempfile import gettempdir
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
 from compat import unittest
 
-from omicron.parameters import validate_parameters
+from omicron.parameters import (validate_parameters, generate_parameters_files)
 
 
 class ParametersTestCase(unittest.TestCase):
@@ -38,3 +45,36 @@ class ParametersTestCase(unittest.TestCase):
         validate_parameters(64, 64, 8)
         validate_parameters(232, 64, 8)
         self.assertRaises(AssertionError, validate_parameters, 128, 65, 8)
+
+    def test_generate_parameters_files(self):
+        remove = []
+        cp = ConfigParser()
+        section = 'test'
+        cp.add_section(section)
+        cp.set(section, 'channels', 'X1:TEST-CHANNEL')
+        # test simple generation
+        remove.extend(
+            generate_parameters_files(cp, section, 'cache.lcf', gettempdir()))
+        # test q-range
+        qrange = (10., 20.)
+        cp.set(section, 'qlow', str(qrange[0]))
+        cp.set(section, 'qhigh', str(qrange[1]))
+        remove.extend(
+            generate_parameters_files(cp, section, 'cache.lcf', gettempdir()))
+        self.assertEqual(cp.get(section, 'q-range'), '%s %s' % qrange)
+        # test multiple channels
+        channels = ['X1:TEST-CHANNEL_%d' % i for i in range(100)]
+        cp.set(section, 'channels', '\n'.join(channels))
+        pars = generate_parameters_files(
+            cp, section, 'cache.lcf', gettempdir())
+        self.assertEqual(len(pars), len(channels) / 10.)
+        pardir = os.path.join(gettempdir(), 'parameters')
+        for i, (f, clist) in enumerate(pars):
+            fpred = os.path.join(pardir, 'parameters_%d.txt' % i)
+            self.assertTrue(os.path.samefile(f, fpred))
+            self.assertListEqual(clist, channels[i*10:(i+1)*10])
+        # clean up
+        rmfiles = set(x[0] for x in remove)
+        for f in rmfiles:
+            if os.path.isfile(f):
+                os.remove(f)
