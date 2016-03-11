c88b26e32ae0c40c5f243fdbe6a804b2247dadd1
Duncan Macleod <duncan.macleod@ligo.org>
2016-03-11 13:07:27 -0800

    tests: use unittest2 for py26

--- omicron/tests/test_utils.py
+++ omicron/tests/test_utils.py
@@ -19,8 +19,13 @@
 """Test parameter handling for Omicron
 """
 
-import unittest
 import os
+import sys
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 from omicron import utils
 
