788d52b414216638f252d4c0726c1341ba5afca4
Duncan Macleod <duncan.macleod@ligo.org>
2018-09-17 11:21:44 +0100

    setup.py: simple improvements to packaging
    main change is to move `requires` to `install_requires`

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (C) Duncan Macleod (2016)
 #
@@ -16,19 +15,21 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyOmicron.  If not, see <http://www.gnu.org/licenses/>.
-# Copyright (C) Gabriele Vajente (2016)
 
 """Setup the PyOmicron package
 """
 
 import glob
 import os.path
+import sys
 
 from setuptools import (setup, find_packages)
 
 import versioneer
 __version__ = versioneer.get_version()
 cmdclass = versioneer.get_cmdclass()
+
+# -- metadata ---------------
 
 # set basic metadata
 PACKAGENAME = 'omicron'
@@ -37,62 +38,88 @@ AUTHOR = 'Duncan Macleod'
 AUTHOR_EMAIL = 'duncan.macleod@ligo.org'
 LICENSE = 'GPLv3'
 
+# read description
+with open('README.md', 'rb') as f:
+    longdesc = f.read().decode().strip()
+
+# -- dependencies -----------
+
+setup_requires = [
+    'setuptools',
+]
+install_requires = [
+    'numpy',
+    'glue',
+    'htcondor',
+    'lal',
+    'gwpy',
+]
+tests_require = [
+    'pytest',
+]
+if {'test'}.intersection(sys.argv):
+    setup_requires.extend([
+        'pytest_runner',
+    ])
+
+# extras
+extras_require = {
+    'doc': [
+        'sphinx',
+        'numpydoc',
+        'sphinxcontrib-programoutput',
+        'sphinxcontrib-epydoc',
+    ],
+}
+
+# -- content ----------------
+
 # Use the find_packages tool to locate all packages and modules
 packagenames = find_packages()
 
 # glob for all scripts
 scripts = glob.glob(os.path.join('bin', '*'))
 
-# -- run setup ----------------------------------------------------------------
 
-setup(name=DISTNAME,
-      provides=[PACKAGENAME],
-      version=__version__,
-      description=None,
-      long_description=None,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      license=LICENSE,
-      packages=packagenames,
-      cmdclass=cmdclass,
-      include_package_data=True,
-      scripts=scripts,
-      setup_requires=[
-          'pytest-runner',
-          'setuptools',
-      ],
-      requires=[
-          'numpy',
-          'glue',
-          'htcondor',
-          'lal',
-          'gwpy',
-          'ROOT',
-      ],
-      install_requires=[
-      ],
-      tests_require=['pytest'],
-      extras_require={
-          'doc': ['sphinx', 'numpydoc', 'sphinxcontrib-programoutput',
-                  'sphinxcontrib-epydoc'],
-      },
-      dependency_links=[
-          'http://software.ligo.org/lscsoft/source/glue-1.49.1.tar.gz',
-      ],
-      use_2to3=True,
-      classifiers=[
-          'Programming Language :: Python',
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Science/Research',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: Developers',
-          'Natural Language :: English',
-          'Topic :: Scientific/Engineering',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics',
-          'Operating System :: POSIX',
-          'Operating System :: Unix',
-          'Operating System :: MacOS',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-      ],
+# -- run setup --------------
+
+setup(
+    # metadata
+    name=DISTNAME,
+    provides=[PACKAGENAME],
+    version=__version__,
+    description="Python utilities and wrappers for GW Omicron",
+    long_description=longdesc,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    cmdclass=cmdclass,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    # dependencies
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    # content
+    packages=packagenames,
+    scripts=scripts,
+    include_package_data=True,
 )
