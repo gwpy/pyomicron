98e12a409c93d31b0191977be2600ca8e7ae6bb0
Joseph Areeda <joseph.areeda@ligo.org>
2022-08-12 07:39:34 -0700

    Add HoldReasonCode 34 to memory bumping expression

--- omicron/cli/process.py
+++ omicron/cli/process.py
@@ -919,7 +919,7 @@ def main(args=None):
     reqmem = condorcmds.pop('request_memory', 1024)
     ojob.add_condor_cmd('+InitialRequestMemory', f'{reqmem}')
     ojob.add_condor_cmd('request_memory', f'ifthenelse(isUndefined(MemoryUsage), {reqmem}, int(3*MemoryUsage))')
-    ojob.add_condor_cmd('periodic_release', '(HoldReasonCode =?= 26) && (JobStatus == 5)')
+    ojob.add_condor_cmd('periodic_release', '(HoldReasonCode =?= 26 || HoldReasonCode =?= 34) && (JobStatus == 5)')
     ojob.add_condor_cmd('periodic_remove', '(JobStatus == 1) && MemoryUsage >= 7G')
 
     ojob.add_condor_cmd('+OmicronProcess', f'"{group}"')
